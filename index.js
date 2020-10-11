3e2a67d32208a3501227d1bc3d7dc0e90038d78c
Vinay Hegde <vinayhegde@outlook.com>
2020-10-11 23:14:57 +0530

    Doc format

--- index.js
+++ index.js
@@ -10,44 +10,44 @@ getUserMedia({ video: true, audio: true }, async function (err, stream) {
     trickle: false,
     stream: stream
   })
-  if(location.hash === '#slave'){
+  if (location.hash === '#slave') {
     isMaster = false;
-    const masterId = await callApi('get',{},'master');
+    const masterId = await callApi('get', {}, 'master');
     peer.signal(masterId)
-  }else if(location.hash === '#master'){
+  } else if (location.hash === '#master') {
     var checkForSlave = setInterval(checkSlave, 1000);
     async function checkSlave() {
-        const slaveId = await callApi('get',{},'slave');
-        if(slaveId){
-            count ++;
-            peer.signal(slaveId);
-            clearInterval(checkForSlave);
-        }
+      const slaveId = await callApi('get', {}, 'slave');
+      if (slaveId) {
+        count++;
+        peer.signal(slaveId);
+        clearInterval(checkForSlave);
+      }
     }
   }
-  peer.on('error', (err)=>{
-      alert(err)
+  peer.on('error', (err) => {
+    alert(err)
   })
   peer.on('signal', function (data) {
-    if(count === 0){
-        callApi('post',{type: isMaster ? 'master' : 'slave', token: JSON.stringify(data)})
+    if (count === 0) {
+      callApi('post', { type: isMaster ? 'master' : 'slave', token: JSON.stringify(data) })
     }
     //document.getElementById('yourId').innerText = JSON.stringify(data)
   })
 
-//   document.getElementById('connect').addEventListener('click', function () {
-//     var otherId = JSON.parse(document.getElementById('otherId').value)
-//     peer.signal(otherId)
-//   })
+    document.getElementById('connect').addEventListener('click', function () {
+      var otherId = JSON.parse(document.getElementById('otherId').value)
+      peer.signal(otherId)
+    })
 
-//   document.getElementById('send').addEventListener('click', function () {
-//     var yourMessage = document.getElementById('yourMessage').value
-//     peer.send(yourMessage)
-//   })
+    document.getElementById('send').addEventListener('click', function () {
+      var yourMessage = document.getElementById('yourMessage').value
+      peer.send(yourMessage)
+    })
 
-//   peer.on('data', function (data) {
-//     document.getElementById('messages').textContent += data + '\n'
-//   })
+    peer.on('data', function (data) {
+      document.getElementById('messages').textContent += data + '\n'
+    })
 
   peer.on('stream', function (stream) {
     const mediaStream = new MediaStream(stream);
@@ -56,19 +56,19 @@ getUserMedia({ video: true, audio: true }, async function (err, stream) {
   })
 })
 
-async function callApi(method = 'get',data={},type=''){
-    try{
-        const options =  {
-            method: method,
-            headers: { 
-                "Content-type": "application/json; charset=UTF-8"
-            } 
-        }
-        method === 'post' ? options['body']= JSON.stringify(data): null;
-        let response = await fetch(`/api?type=${type}`,options)
-        response = await response.json()
-        return response.token;
-    }catch(err){
-        return null
+async function callApi(method = 'get', data = {}, type = '') {
+  try {
+    const options = {
+      method: method,
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
     }
+    method === 'post' ? options['body'] = JSON.stringify(data) : null;
+    let response = await fetch(`/api?type=${type}`, options)
+    response = await response.json()
+    return response.token;
+  } catch (err) {
+    return null
+  }
 }
